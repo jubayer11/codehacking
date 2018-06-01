c2a39f6d5ca41a0c3ab50acb9f1358383fd3939e
jubayer <ahmedjubayer54@gmail.com>
2018-06-01 10:12:08 +0600

    users working without photos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,39 @@
- <h1> hello there</h1>
+@extends('layouts.admin')
+
+
+
+@section('content')
+    <h1>users</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>created_at</th>
+            <th>updated_at</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role['name']}}</td>
+                    <td>{{$user->is_active== 1 ? 'Active' : 'NO ACTIVE'}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+        </tbody>
+    </table>
+
+
+@stop
