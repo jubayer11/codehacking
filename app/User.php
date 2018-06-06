62cae65214d971cf4b128c79dcc55c862d7404c1
jubayer <ahmedjubayer54@gmail.com>
2018-06-06 12:41:53 +0600

    able to create post with displaying image

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
         return false;
 
     }
+    public function posts(){
+
+        return $this->hasMany('App\post');
+    }
 }
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+@section('content')
+
+    <h1> posts  </h1>
+     <table class="table">
+         <thead>
+           <tr>
+             <th>Id</th>
+               <th>OWNER</th>
+               <th>Category id</th>
+               <th>Photo </th>
+               <th>Title</th>
+               <th>body</th>
+               <th>Created</th>
+               <th>Updated</th>
+           </tr>
+         </thead>
+         <tbody>
+         @if($posts)
+             @foreach($posts as $post)
+           <tr>
+             <td>{{$post->id}}</td>
+               <td><img height="100px" src=" {{ URL::to('/') }}/images/{{$post->photo ? $post->photo['file']: 'no user photo'}}" alt="no photo"></td>
+             <td>{{$post->user->name}}</td>
+             <td>{{$post->category_id}}</td>
+               <td> {{$post->title}}</td>
+               <td>{{$post->body}}</td>
+               <td>{{$post->created_at->diffForhumans()}}</td>
+               <td> {{$post->updated_at->diffForhumans()}}</td>
+           </tr>
+             @endforeach
+             @endif
+         </tbody>
+       </table>
+    @stop
